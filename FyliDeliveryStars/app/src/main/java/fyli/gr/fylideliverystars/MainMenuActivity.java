88282aab37bcb32d5ede0ac7374b971e5ebd8417
Zarkopafilis <zarkopafilis@gmail.com>
2016-03-28 14:43:06 +0300

    Add round menu buttons with some foundative calculations Pentagon placement pending

--- FyliDeliveryStars/app/src/main/java/fyli/gr/fylideliverystars/MainMenuActivity.java
+++ FyliDeliveryStars/app/src/main/java/fyli/gr/fylideliverystars/MainMenuActivity.java
@@ -4,21 +4,19 @@ import android.app.Activity;
 import android.app.ActivityOptions;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
-import android.view.Gravity;
+import android.util.Log;
+import android.view.Display;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
 
 
-public class MainMenuActivity extends Activity implements AdapterView.OnItemClickListener{
+public class MainMenuActivity extends Activity{
 
 
     @Override
@@ -29,16 +27,120 @@ public class MainMenuActivity extends Activity implements AdapterView.OnItemClic
 
         setContentView(R.layout.activity_main_menu);
 
+        final Context self = this;
 
-        //Show welcoming toast
+        //Get screen dimensions
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int screenWidth = size.x;
+        int screenHeight = size.y;
+        int buttonDiameter = (int)( Math.max(screenHeight, screenWidth) * 0.15 + Math.min(screenHeight , screenWidth) * 0.2) / 2;
+        Log.d("slp" , "buttonDiameter:" + buttonDiameter);
+
+        //Σουβλάκι => 8 chars
+        //Pizza => 5 chars
+        //Burger => 6 chars
+        //Κρέπα => 5 chars
+        //Καφές => 5 chars
+        //The text size needs to fit the biggest word (8 chars)
+        int textSize = (int) ((int) buttonDiameter/(8*2));
+
+
+        Button souvlakiButton = (Button) findViewById(R.id.souvlakiButton);
+        Button pizzaButton = (Button) findViewById(R.id.pizzaButton);
+        Button burgerButton = (Button) findViewById(R.id.burgerButton);
+        Button pancakeButton = (Button) findViewById(R.id.pancakeButton);
+        Button coffeeButton = (Button) findViewById(R.id.coffeeButton);
+
+        //Buttons are circles
+        RelativeLayout.LayoutParams souvlakiParams = new RelativeLayout.LayoutParams(buttonDiameter , buttonDiameter);
+        souvlakiButton.setLayoutParams (souvlakiParams);
+        souvlakiButton.setTextSize(textSize);
+
+        RelativeLayout.LayoutParams pizzaParams = new RelativeLayout.LayoutParams(buttonDiameter , buttonDiameter);
+        pizzaButton.setLayoutParams(pizzaParams);
+        pizzaButton.setTextSize(textSize);
+
+        RelativeLayout.LayoutParams burgerParams = new RelativeLayout.LayoutParams(buttonDiameter , buttonDiameter);
+        burgerButton.setLayoutParams(burgerParams);
+        burgerButton.setTextSize(textSize);
+
+        RelativeLayout.LayoutParams pancakeParams = new RelativeLayout.LayoutParams(buttonDiameter , buttonDiameter);
+        pancakeButton.setLayoutParams(pancakeParams);
+        pancakeButton.setTextSize(textSize);
+
+        RelativeLayout.LayoutParams coffeeParams = new RelativeLayout.LayoutParams(buttonDiameter , buttonDiameter);
+        coffeeButton.setLayoutParams (coffeeParams);
+        coffeeButton.setTextSize(textSize);
+
+
+        //Listener setup
+        souvlakiButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                launchShopLister("souvlaki");
+            }
+        });
+
+        pizzaButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                launchShopLister("pizza");
+            }
+        });
+
+        burgerButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                launchShopLister("burger");
+            }
+        });
+
+        pancakeButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                launchShopLister("pancake");
+            }
+        });
+
+        coffeeButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                launchShopLister("coffee");
+            }
+        });
+
+
+
+        //set up contact listener
+        TextView contactText = (TextView) findViewById(R.id.contactText);
+        final Activity thisActivity = this;
+        contactText.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(self , Contact.class);
+                startActivity(intent,
+                        ActivityOptions
+                                .makeSceneTransitionAnimation(thisActivity).toBundle());
+            }
+        });
+
+        /*//Show welcoming toast
         Context context = getApplicationContext();
         CharSequence welcome_text = getResources().getString(R.string.welcome_toast);
         int duration = Toast.LENGTH_SHORT;
         final Toast toast = Toast.makeText(context, welcome_text, duration);
         toast.setGravity(Gravity.CENTER,0,0);
-        toast.show();
+        toast.show();*/
 
-        //Shop types
+        /*//Shop types
         ArrayList<String> shopTypeArray = new ArrayList<String>();
         shopTypeArray.add("Σουβλάκι");
         shopTypeArray.add("Pizza");
@@ -46,33 +148,26 @@ public class MainMenuActivity extends Activity implements AdapterView.OnItemClic
         shopTypeArray.add("Κρέπα");
         shopTypeArray.add("Καφές");
 
-
         //Set the listview
         ListView shopTypeListView = (ListView) findViewById(R.id.ShopTypeList);
         shopTypeListView.setOnItemClickListener(this);
 
         ArrayAdapter<String> shopTypeListAdapter = new ArrayAdapter<String>(getApplicationContext(),R.layout.list_item_simple_black_tv, shopTypeArray);
         shopTypeListView.setAdapter(shopTypeListAdapter);
-        shopTypeListAdapter.notifyDataSetChanged();
-
-        //set up contact listener
-        TextView contactText = (TextView) findViewById(R.id.contactText);
-
-        final Context self = this;
-
-        contactText.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(self , Contact.class);
-                final Context c = self.getApplicationContext();
-                startActivity(intent);
-            }
-        });
+        shopTypeListAdapter.notifyDataSetChanged();*/
 
     }
 
-    public void onItemClick(AdapterView<?> l, View v, int position, long id) {
+    private void launchShopLister(String shopType){
+        Intent intent = new Intent();
+        intent.setClass(this, DeliveryOrTakeAway.class);
+        intent.putExtra("shopType" , shopType);
+        startActivity(intent,
+                ActivityOptions
+                        .makeSceneTransitionAnimation(this).toBundle());
+    }
+
+    /*public void onItemClick(AdapterView<?> l, View v, int position, long id) {
 
         String shopType = "souvlaki";
 
@@ -103,7 +198,7 @@ public class MainMenuActivity extends Activity implements AdapterView.OnItemClic
         intent.setClass(this, DeliveryOrTakeAway.class);
         intent.putExtra("shopType" , shopType);
         startActivity(intent);
-    }
+    }*/
 
     //Don't go to the splash screen and cause hang
     @Override
